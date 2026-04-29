ffcae835926e623bd9ad991e9e03363a642ebf6a
Daniel Calla Castillo <danielcallacastillo7u7@gmail.com>
2026-04-29 12:06:53 -0500

    CAMBIAZO

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const app = express()
 
 // Middlewares
 app.use(cors({
-    origin: 'https://club-pi-swart.vercel.app',
+    origin: 'https://club-pi-swart.vercel.app/',
     credentials: true
 }))
 app.use(express.json({ limit: '50mb' }));
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log("Servidor corriendo en puerto " + PORT);
 })
